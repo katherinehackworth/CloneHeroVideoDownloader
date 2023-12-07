7d7836c4c671c3eb5aa98b647b42dca641ae744d
stripedew <98356696+stripedew@users.noreply.github.com>
2023-12-07 22:25:50 +0000

    Update VideoDownload.py
    Added several new quality options, as well as remuxing

--- VideoDownload.py
+++ VideoDownload.py
@@ -1,8 +1,10 @@
+from codecs import ignore_errors
 import configparser
 import glob
 import os
 import time
 import yt_dlp
+import ffmpeg
 from tqdm import tqdm
 from youtubesearchpython import VideosSearch
 
@@ -10,18 +12,57 @@ def clean_cookie():
 	if os.path.exists(".google-cookie"):
 		os.remove(".google-cookie")
 
+def download_video(ydl, url):
+	ydl.download([url])
+	# Remux video into a format Clone Hero can play
+	if (videoQuality != 'mp4'):
+		print('Formatting downloaded video for Clone Hero')
+		try:
+			stream = ffmpeg.input('video.mp4')
+			stream = ffmpeg.output(stream, 'output.mp4', vcodec='copy')
+			ffmpeg.run(stream, overwrite_output=True, quiet=True)
+			os.remove('video.mp4')
+			os.rename('output.mp4', 'video.mp4')
+		except Exception as e:
+			print('Error while converting video. Error: ' + str(e))
+		else:
+			print('Video ready')
+
 clean_cookie()		
 
-print('Checking for home folder')
+print('Checking for home folder...')
 songsFolder = os.getcwd() + "\\songs"
-
+time.sleep (0.5)
 if os.path.exists(songsFolder):
-	print('Songs folder found')
+	print('Songs folder found\n')
+	time.sleep (0.5)
+	replace = 'false'
+	videoQuality = '720p'
+
+	qualityInput = input('Type the number to pick from the following options:\n'
+					  + '1. Default quality (720p)\n'
+					  + '2. Best quality (1080p, where available; significantly bigger files):\n'
+					  + '3. [EXPERIMENTAL] Replace existing videos with 1080p (Caution: Use at your own risk. May malfunction and delete videos)\n\n'
+					  + 'Pick between 1-3: ')
+	
+	if (qualityInput == '1'):
+		print('Set to 720p')
+		videoQuality = 'mp4'
+	elif (qualityInput == '2'):
+		print('Set to 1080p. Poor hard drive!')
+		videoQuality = 'bestvideo[vcodec^=avc]/best[ext=mp4]/best'
+	elif (qualityInput == '3'):
+		print('Replacing all videos with 1080p. You have time for a nap!')
+		videoQuality = 'bestvideo[vcodec^=avc]/best[ext=mp4]/best'
+		replace = 'true'
+	else:
+		print('You must choose between 1-3. Try again')
+		exit()
+
 	homeFolder = os.path.abspath(os.getcwd() + "\\songs")
 	os.chdir(homeFolder)
-
-	videoTitle=''
-	i=0
+	videoTitle = ''
+	i = 0
 	erroredSongs = []
 	erroredSongNames = []
 
@@ -29,18 +70,20 @@ if os.path.exists(songsFolder):
 		i+=1
 
 	totalcount = i
-
+	
 	while True:
 		with tqdm(total=i,unit="videos") as pbar:
-			try:
-				for filename in glob.iglob(homeFolder + "/**/song.ini", recursive=True):
-					currentSongFileFolder = os.path.dirname(filename)
-					currentSongName = os.path.basename(currentSongFileFolder)
-					os.chdir(currentSongFileFolder)
-					time.sleep (0.0001)
-					pbar.update(1)
+			for filename in glob.iglob(homeFolder + "/**/song.ini", recursive=True):
+				currentSongFileFolder = os.path.dirname(filename)
+				currentSongName = os.path.basename(currentSongFileFolder)
+				os.chdir(currentSongFileFolder)
+				time.sleep (0.0001)
+				pbar.update(1)
 
-					if not os.path.exists("video.mp4") and not currentSongName in erroredSongNames:
+				if (not os.path.exists("video.mp4") and not currentSongName in erroredSongNames) or replace == 'true':
+					try:
+						if os.path.exists('video.mp4'):
+							os.remove('video.mp4')
 						query = '{} (Official Music Video)'.format(currentSongName)
 						print('\nLooking on YouTube for: ' + query)
 
@@ -49,58 +92,55 @@ if os.path.exists(songsFolder):
 						url = youtube['result'][0]['link']
 						url2 = youtube['result'][1]['link']
 						videoTitle = youtube['result'][0]['title']
-						print("\nSearch success. Now downloading: " + videoTitle)
+						print("Search success. Now downloading: " + videoTitle)
 
 						# downloads the song
 						ydl_opts = {'outtmpl': 'video.mp4',
-								'format': 'mp4',
+								'format': videoQuality,
 								'nooverwrites': 0,
 								'noplaylist': 1,
 								'quiet': True}
+						
 						with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 							try:
-								ydl.download([url])
-								print('Now downloaded')
+								download_video(ydl, url)
 							except Exception as e:
 								print('Error while downloading: ' + str(e) + '. Trying second video')
 								# Try second video if first video errors
-								ydl.download([url2])
+								download_video(ydl, url2)
 						with open('song.ini') as songCheck:
 							# check if the ini file contains unexpected phase shift converter text
 							if '//Converted' in songCheck.read():
 								erroredSongs.append(filename)
-							else: 
+							else:
 								# change the song.ini file to attempt to sync the video
 								config = configparser.ConfigParser()
 								config.read('song.ini')
 								# Check uppercase/lowercase config section name
 								if config.has_section('song'):
 									config.set('song', 'video_start_time', '-3000')
+									print('Song ready. Next song...\n')
 								elif config.has_section('Song'):
 									config.set('Song', 'video_start_time', '-3000')
+									print('Song ready. Next song...\n')
 								else:
-									print('Could not update song.ini. Check the song.ini for potential issues')
+									print('Could not update song.ini. Check the song.ini for potential issues\n')
 									erroredSongs.append(filename)
 								with open('song.ini', 'w') as configfile:
 									config.write(configfile)
 						clean_cookie()
-			except Exception as e:
-				print(e)
-				print("Error downloading song: " + currentSongName + ". Skipping")
-				erroredSongs.append(videoTitle)
-				erroredSongNames.append(currentSongName)
-				continue
+					except Exception as e:
+						print(e)
+						print("Error downloading song: " + currentSongName + ". Skipping")
+						erroredSongs.append(videoTitle)
+						erroredSongNames.append(currentSongName)
+						continue
 			else:
-				break
-
+				break			
 	if erroredSongs:
 		print("The following videos were downloaded but not audio-synced")
 		for i in erroredSongs:
-
 			print(i, end='\n')
-	input("All downloads complete. Checked a total of " + str(totalcount)+ " songs. Press any button to exit.")
+	input("All downloads complete. Checked a total of " + str(totalcount)+ " songs. Press Enter button to exit.")
 else:
 	input("Did not detect a 'Songs' folder. Check you have placed the .exe file in the directory one level above it. Press any button to exit")
-
-
-
